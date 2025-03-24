e52178099cdd9be2fced51802364a7c20da836ef
jonaksindhu <40793270+jonaksindhu@users.noreply.github.com>
2025-03-24 15:20:39 +0530

    added changes to code

--- tests/DSAproblems1Test.java
+++ tests/DSAproblems1Test.java
@@ -1,6 +1,10 @@
 package tests;
 
-public @Test
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DSAproblems1Test{
+@Test
 public void testMatrixMultiplication() {
     int[][] A = {{1, 2}, {3, 4}};
     int[][] B = {{2, 0}, {1, 2}};
@@ -18,6 +22,7 @@ public void testRotateArray() {
 @Test
 public void testReverseNumber() {
     assertEquals(54321, DSAproblems1.reverseNumber(12345));
-} {
-    
+} 
 }
+    
+
